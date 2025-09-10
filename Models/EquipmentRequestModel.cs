50a32dc48434c5d176b054469f17d0410e9e0bcb
Matthew Eastwood <eastwmat@sheridancollege.ca>
2025-09-10 14:54:05 -0400

    created equipment request model

---
+++ Models/EquipmentRequestModel.cs
@@ -0,0 +1,13 @@
+namespace Assignment1.models
+{
+    public class EquipmentRequestModel
+    {
+        public string? name { get; set; }
+        public string? email { get; set; }
+        public string? phoneNumber { get; set; }
+        public string? role { get; set; }
+        public string? equipmentType { get; set; }
+        public string? details { get; set; }
+        public int? duration { get; set; }
+    }
+}
