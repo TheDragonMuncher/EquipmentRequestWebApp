f0179b5de7d396c579f7fa52137d6eb5de272d70
Matthew Eastwood <eastwmat@sheridancollege.ca>
2025-09-10 21:26:01 -0400

    added some spaces in the request model

--- Models/EquipmentRequestModel.cs
+++ Models/EquipmentRequestModel.cs
@@ -7,23 +7,30 @@ namespace Assignment1.models
         private static int idCounter = 0;
 
         public int id = 0;
+
         [Required(ErrorMessage = "Please enter your name.")]
         public string? name { get; set; }
+
         [Required(ErrorMessage = "Please enter a valid email address.")]
         [EmailAddress]
         public string? email { get; set; }
-        // [Required(ErrorMessage = "Please enter a valid phone number.")]
+
         [RegularExpression(@"^\d{3}-\d{3}-\d{4}$", ErrorMessage = "Please enter a valid phone number.")]
+
         public string? phoneNumber { get; set; }
         [Required(ErrorMessage = "Please select a role.")]
         public string? role { get; set; }
+
         [Required(ErrorMessage = "Please select an equipment type.")]
         public string? equipmentType { get; set; }
+
         [Required(ErrorMessage = "Please describe the reason for your request.")]
         public string? details { get; set; }
+
         [Required(ErrorMessage = "Please enter a positive duration for the request.")]
         [Range(1, int.MaxValue, ErrorMessage = "Please enter a positive duration for the request.")]
         public int? duration { get; set; }
+
 
         public void GiveId()
         {
