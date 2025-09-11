5700fa664f6806bd0648d5003d78828845bb495e
Matthew Eastwood <eastwmat@sheridancollege.ca>
2025-09-10 21:03:47 -0400

    Added content to confirmation page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,6 +22,6 @@ public class HomeController : Controller
     {
         request.GiveId();
         Repository.AddRequest(request);
-        return View("Confirmation", request);
+        return View("RequestConfirmation", request);
     }
 }
