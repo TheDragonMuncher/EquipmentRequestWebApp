f913556320aafe9eb14c778dec612893d241e2bd
Matthew Eastwood <eastwmat@sheridancollege.ca>
2025-09-11 08:35:27 -0400

    populate equipment list in repository

--- Models/EquipmentModel.cs
+++ Models/EquipmentModel.cs
@@ -2,17 +2,9 @@ namespace Assignment1.Models
 {
     public class EquipmentModel
     {
-        private static int idCounter = 0;
-
         public int id { get; set; }
         public string? type { get; set; }
         public string? description { get; set; }
         public bool available { get; set; }
-
-        public void GiveId()
-        {
-            idCounter++;
-            id = idCounter;
-        }
     }
 }
--- Models/Repository.cs
+++ Models/Repository.cs
@@ -5,7 +5,62 @@ namespace Assignment1.Models
         private static List<EquipmentRequestModel> requests = new();
         public static IEnumerable<EquipmentRequestModel> Requests => requests;
 
-        private static List<EquipmentModel> equipmentList = new();
+        private static List<EquipmentModel> equipmentList = [
+            new EquipmentModel {
+                id = 1,
+                type = "Laptop",
+                description = "Portable Workspace",
+                available = true
+            },
+            new EquipmentModel {
+                id = 2,
+                type = "Laptop",
+                description = "Portable Workspace",
+                available = true
+            },
+            new EquipmentModel {
+                id = 3,
+                type = "Laptop",
+                description = "Portable Workspace",
+                available = true
+            },
+            new EquipmentModel {
+                id = 4,
+                type = "Phone",
+                description = "For Work Calls",
+                available = true
+            },
+            new EquipmentModel {
+                id = 5,
+                type = "Phone",
+                description = "For Work Calls",
+                available = true
+            },
+            new EquipmentModel {
+                id = 6,
+                type = "Phone",
+                description = "For Work Calls",
+                available = true
+            },
+            new EquipmentModel {
+                id = 7,
+                type = "Tablet",
+                description = "For The Kid(s)",
+                available = true
+            },
+            new EquipmentModel {
+                id = 8,
+                type = "Tablet",
+                description = "For The Kid(s)",
+                available = true
+            },
+            new EquipmentModel {
+                id = 9,
+                type = "Tablet",
+                description = "For The Kid(s)",
+                available = true
+            }
+        ];
         public static IEnumerable<EquipmentModel> Equipment => equipmentList;
 
 
@@ -13,11 +68,6 @@ namespace Assignment1.Models
         {
             Console.WriteLine(request);
             requests.Add(request);
-        }
-
-        public static void AddEquipment(EquipmentModel equipment)
-        {
-            equipmentList.Add(equipment);
         }
     }
 }
